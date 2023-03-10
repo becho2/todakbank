e8d0f9a9835a319d7cd236455aa8aa597187dae0
jinhoe <jinhoe.kim@imweb.me>
2023-03-10 14:23:51 +0900

    feat: mysql2, nestjsconfig 설치, 기본테스트 깨지는 문제 provider 추가로 해결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
+// import { knex } from 'knex';
 
 @Module({
   imports: [],
