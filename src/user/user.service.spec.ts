bac3277153e5bcf8d81165dc07c2d8b6fea2423b
jinhoe <jinhoe.kim@imweb.me>
2023-03-14 08:57:43 +0900

    test: User module 유닛테스트에 EmailService를 프로바이더로 추가

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,14 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { EmailVerificationDto } from './dto/email-verification.dto';
+import { EmailService } from '../email/email.service';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [UserService, EmailService],
     }).compile();
 
     service = module.get<UserService>(UserService);
