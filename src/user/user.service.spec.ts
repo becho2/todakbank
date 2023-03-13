e3f2863bc1e5a77008b29aef9422132d2bafd44c
jinhoe <jinhoe.kim@imweb.me>
2023-03-13 13:55:21 +0900

    test: add unit tests

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { EmailVerificationDto } from './dto/email-verification.dto';
 
 describe('UserService', () => {
   let service: UserService;
@@ -15,4 +17,25 @@ describe('UserService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should be able to create', () => {
+    const user = service.createUser({
+      id: 'John',
+      name: 'Doe',
+      phoneNumber: '01012345678',
+      email: 'upchh@example.com',
+      age: 18
+    });
+    expect(user).toEqual(user);
+  })
+
+  it('should be able to send verification email', () => {
+    const result = service.sendEmailVerification({
+      id: 'John',
+      name: 'Doe',
+      phoneNumber: '01012345678',
+      email: 'upchh@example.com',
+    });
+    expect(result).toBeTruthy();
+  })
 });
