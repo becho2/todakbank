c123586ed5d049b2afa950e1bdd659604404746e
jinhoe <jinhoe.kim@imweb.me>
2023-04-23 14:11:45 +0900

    npm 쓰다가 실수로 yarn명령어를 써서 yarn 파일들 생성, transform trim 제거

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,14 @@
-import { IsString, IsEmail, IsInt, Min, Max, MinLength, MaxLength, IsPhoneNumber, Matches } from 'class-validator';
+import {
+  IsString,
+  IsEmail,
+  IsInt,
+  Min,
+  Max,
+  MinLength,
+  MaxLength,
+  IsPhoneNumber,
+  Matches,
+} from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 import { BadRequestException } from '@nestjs/common';
@@ -10,18 +20,12 @@ export class CreateUserDto {
   @ApiProperty({ description: 'The user id', example: 'johndoe123' })
   id: string;
 
-  @Transform(({value, obj}) => {
-    if (obj.password.includes(obj.name.trim()) || obj.password.includes(obj.id.trim())) {
-      throw new BadRequestException('password는 name 또는 id를 포함할 수 없습니다.');
-    }
-    return value.trim();
-  })
   @IsString()
   @Matches(/^[a-zA-Z\d!@#$%^&*()]{8,30}$/)
   @ApiProperty({ description: 'The password', example: 'v@ryd1ff1cu1t' })
   password: string;
 
-  @Transform(params => params.value.trim())
+  @Transform((params) => params.value.trim())
   @IsString()
   @MinLength(3)
   @MaxLength(25)
@@ -29,7 +33,10 @@ export class CreateUserDto {
   name: string;
 
   @IsPhoneNumber('KR')
-  @ApiProperty({ description: 'The user phone number', example: '010-1234-5674' })
+  @ApiProperty({
+    description: 'The user phone number',
+    example: '010-1234-5674',
+  })
   phoneNumber: string;
 
   @IsEmail()
